d0f928506936fe072ec79179fd2cb77cb5e41bcf
Gemma McCombe <gemma.mccombe@theaccessgroup.com>
2025-12-29 14:06:57 +0000

    add hotel block

--- next-react-project/src/app/hotels/page.js
+++ next-react-project/src/app/hotels/page.js
@@ -1,3 +1,5 @@
+import HotelBlock from "./HotelBlock";
+
 async function getData() {
   const res = await fetch(
     "https://snowtooth-api-rest.fly.dev"
@@ -5,14 +7,7 @@ async function getData() {
   return res.json();
 }
 
-function HotelBlock({name, capacity}){
-  return(
-    <div>
-      <h2>{name}</h2>
-      <p>{capacity}</p>
-    </div>
-  )
-}
+
 
 export default async function Page() {
   const data = await getData();
@@ -23,7 +18,11 @@ export default async function Page() {
         {/* <div>{JSON.stringify(data)}</div> */}
         <div>
           {data.map((hotel) => (
-          <HotelBlock key={hotel.id} name={hotel.name} capacity={hotel.capacity}/>
+          <HotelBlock 
+          key={hotel.id} 
+          id={hotel.id}
+          name={hotel.name} 
+          capacity={hotel.capacity}/>
         ))}
         </div>
       </div>
