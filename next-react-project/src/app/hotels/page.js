cd8c08adbaa866df7229725aba21920eb9b3c6e8
Gemma McCombe <gemma.mccombe@theaccessgroup.com>
2025-12-29 13:53:19 +0000

    hotel details from API

--- next-react-project/src/app/hotels/page.js
+++ next-react-project/src/app/hotels/page.js
@@ -1,8 +1,31 @@
-export default function Page() {
-  return (
+async function getData() {
+  const res = await fetch(
+    "https://snowtooth-api-rest.fly.dev"
+  );
+  return res.json();
+}
+
+function HotelBlock({name, capacity}){
+  return(
+    <div>
+      <h2>{name}</h2>
+      <p>{capacity}</p>
+    </div>
+  )
+}
+
+export default async function Page() {
+  const data = await getData();
+  return (    
     <main>
       <div>
         <h1>Hotel Details</h1>
+        {/* <div>{JSON.stringify(data)}</div> */}
+        <div>
+          {data.map((hotel) => (
+          <HotelBlock key={hotel.id} name={hotel.name} capacity={hotel.capacity}/>
+        ))}
+        </div>
       </div>
     </main>
   );
